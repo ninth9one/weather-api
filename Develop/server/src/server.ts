2c1eca6dc3e1fd9b3525ef20a2d92efc24d758cd
ninth9one <crlongsworth0@gmail.com>
2024-12-13 20:51:11 -0500

    server port

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -10,9 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-
+const staticPath = path.join(__dirname, '../../client/dist');
+app.use(express.static(staticPath));
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
